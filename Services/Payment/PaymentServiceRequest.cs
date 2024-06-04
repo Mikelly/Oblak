16bc960b075e2808ed03f958389107966ba0256c
lukabozinovic <luka.bozinovic.dev@gmail.com>
2024-06-05 00:00:04 +0200

    fixed payment request being sent with null values

--- Services/Payment/PaymentServiceRequest.cs
+++ Services/Payment/PaymentServiceRequest.cs
@@ -6,5 +6,8 @@
         public decimal Amount { get; set; }
         public decimal SurchargeAmount { get; set; }
         public string TransactionToken { get;set; } = string.Empty;
+        public string SuccessUrl { get; set; } = string.Empty;
+        public string CancelUrl { get; set; } = string.Empty;
+        public string ErrorUrl { get; set; } = string.Empty;
     }
 }

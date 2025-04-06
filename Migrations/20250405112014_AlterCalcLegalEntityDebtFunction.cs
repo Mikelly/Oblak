7afcce24f9334690ad5c193566f8ca076d7864c8
milija.ivovic <milija.ivovic@outlook.com>
2025-04-06 20:42:53 +0200

    Update fun za obracun duga starijeg od 30 dana i update entiteta Agency.

---
+++ Migrations/20250405112014_AlterCalcLegalEntityDebtFunction.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Oblak.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterCalcLegalEntityDebtFunction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+            ALTER FUNCTION [dbo].[CalcLegalEntityDebt]
+            (
+                @legalEntityId INT,
+                @partner INT = 4 
+            )
+            RETURNS DECIMAL(18,2)
+            AS
+            BEGIN
+                DECLARE @Saldo DECIMAL(18,2) = 0;
+                WITH SaldoData AS 
+                (
+                    -- MnePersons
+                    SELECT 
+                        m.LegalEntityId AS Id,
+                        m.ResTaxAmount AS Duguje,
+                        CASE 
+                            WHEN rp.PaymentStatus = 'Cash' THEN m.ResTaxAmount 
+                            WHEN rp.PaymentStatus = 'AlreadyPaid' THEN pay.Amount
+                            WHEN rp.PaymentStatus = 'Unpaid' THEN 0
+                        END AS Potrazuje
+                    FROM MnePersons m
+                    INNER JOIN Properties p ON p.Id = m.PropertyId
+                    INNER JOIN LegalEntities le ON le.Id = p.LegalEntityId
+                    INNER JOIN ResTaxPaymentTypes rp ON rp.Id = m.ResTaxPaymentTypeId
+                    LEFT JOIN TaxPayments pay ON pay.PersonId = m.Id
+                    WHERE le.PartnerId = @partner
+                        AND le.Id = @legalEntityId
+                        AND m.GroupId IS NULL
+                        AND m.ResTaxAmount > 0
+                        AND CAST(m.UserCreatedDate AS DATE) < GETDATE() - 30
+
+                    UNION ALL
+
+                    -- Groups
+                    SELECT 
+                        g.LegalEntityId AS Id,
+                        g.ResTaxAmount AS Duguje,
+                        CASE 
+                            WHEN rp.PaymentStatus = 'Cash' THEN g.ResTaxAmount 
+                            WHEN rp.PaymentStatus = 'AlreadyPaid' THEN pay.Amount
+                            WHEN rp.PaymentStatus = 'Unpaid' THEN 0
+                        END AS Potrazuje
+                    FROM Groups g
+                    INNER JOIN Properties p ON p.Id = g.PropertyId
+                    INNER JOIN LegalEntities le ON le.Id = p.LegalEntityId
+                    INNER JOIN ResTaxPaymentTypes rp ON rp.Id = g.ResTaxPaymentTypeId
+                    LEFT JOIN TaxPayments pay ON pay.GroupId = g.Id
+                    WHERE le.PartnerId = @partner
+                        AND le.Id = @legalEntityId
+                        AND g.ResTaxAmount > 0
+                        AND CAST(g.UserCreatedDate AS DATE) < GETDATE() - 30
+
+                    UNION ALL
+
+                    -- TaxPayments
+                    SELECT 
+                        pay.LegalEntityId AS Id,
+                        0 AS Duguje,
+                        pay.Amount AS Potrazuje
+                    FROM TaxPayments pay
+                    INNER JOIN LegalEntities le ON le.Id = pay.LegalEntityId
+                    INNER JOIN TaxPaymentTypes t ON t.Id = pay.TaxPaymentTypeId
+                    WHERE pay.PersonId IS NULL
+                        AND le.Id = @legalEntityId
+                        AND pay.GroupId IS NULL 
+                )
+                SELECT @Saldo = SUM(Duguje - Potrazuje) FROM SaldoData;
+                RETURN @Saldo;
+            END
+        """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Migrations/20250405112212_AlterTouristOrgDebtProc.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Oblak.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterTouristOrgDebtProc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                ALTER PROCEDURE [dbo].[TouristOrgDebt]
+                	 @partner int = 4 
+                AS
+                BEGIN
+
+
+                	select le.[name] Izdavaoc, p.[name] Apartman, p.[address] Adresa, cl.[name] Mjesto, b.Saldo, le.PhoneNumber from
+
+                	(
+
+                	select Id, SUM(Duguje - Potrazuje) Saldo
+
+                	from
+
+                		(
+                			select Id, sum(duguje) duguje, sum(potrazuje) potrazuje from 
+                			(select le.Id, m.ResTaxAmount Duguje, 
+                				CASE 
+                					WHEN rp.PaymentStatus = 'Cash' THEN m.ResTaxAmount 
+                					WHEN rp.PaymentStatus = 'AlreadyPaid' THEN (SELECT pay.Amount FROM TaxPayments pay WHERE pay.PersonId = m.Id)
+                					WHEN rp.PaymentStatus = 'Unpaid' THEN 0
+                				END Potrazuje 
+                			from MnePersons m inner join Properties p on p.Id = m.PropertyId inner join LegalEntities le on le.Id = p.LegalEntityId inner join ResTaxPaymentTypes rp on rp.Id = m.ResTaxPaymentTypeId
+                			where le.PartnerId  = @partner and m.GroupId is null and m.ResTaxAmount > 0 and CAST(m.UserCreatedDate as date) < getdate()-30 ) aa
+                			group by Id
+
+                			union all
+
+                			select Id, sum(duguje) duguje, sum(potrazuje) potrazuje from 
+                			(select le.Id, 
+                				g.ResTaxAmount Duguje, 
+                				CASE 
+                					WHEN rp.PaymentStatus = 'Cash' THEN g.ResTaxAmount 
+                					WHEN rp.PaymentStatus = 'AlreadyPaid' THEN (SELECT pay.Amount FROM TaxPayments pay WHERE pay.GroupId = g.Id)
+                					WHEN rp.PaymentStatus = 'Unpaid' THEN 0
+                				END Potrazuje 
+                			from Groups g inner join Properties p on p.Id = g.PropertyId inner join LegalEntities le on le.Id = p.LegalEntityId inner join ResTaxPaymentTypes rp on rp.Id = g.ResTaxPaymentTypeId
+                			where le.PartnerId  = @partner and g.ResTaxAmount > 0 and CAST(g.UserCreatedDate as date) < getdate()-30 ) bb
+                			group by Id
+
+                			union all
+
+                			select pay.LegalEntityId Id, 0 duguje, sum(pay.Amount) potrazuje
+                			from TaxPayments pay inner join LegalEntities le on le.Id = pay.LegalEntityId inner join TaxPaymentTypes t on t.Id = pay.TaxPaymentTypeId
+                			where pay.PersonId is null and pay.GroupId is null 
+                			group by  pay.LegalEntityId
+
+                		) a
+                		group by Id
+                	) b 
+                	inner join LegalEntities le on le.Id = b.Id
+                	inner join Properties p on p.LegalEntityId = b.Id 
+                	inner join CodeLists cl on cl.ExternalId = p.Place
+                	where saldo > 0 order by b.saldo desc
+
+
+                END
+                
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
